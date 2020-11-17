38be4fed67885e24b059e5fd1ea78a73d68cdcb4
Angel-Benita <55314287+Angel-Benita@users.noreply.github.com>
2020-11-17 22:36:43 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,3 +1,5 @@
+//TCP CLIENT 
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -22,3 +24,38 @@ public class TCPClient
            System.out.println("FILE IS EMPTY");
     }
  }
+
+//SERVER SIDE
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+public class TCPServer
+  {
+   public static void main(String[] args) throws IOException
+     {
+      ServerSocket server;
+      DataOutputStream out = null;
+      DataInputStream in;
+      try
+        {
+         server = new ServerSocket(6000, 1); //port number and num of connections
+         System.out.println("Server Waiting for client");
+         Socket socket = server.accept();
+         System.out.println("Client connected ");
+         in = new DataInputStream(socket.getInputStream());
+         out = new DataOutputStream(socket.getOutputStream());
+         String fileName = in.readUTF();
+         System.out.println("File Requested is : " + fileName);
+         byte[] filedata = Files.readAllBytes(Paths.get(fileName));
+         String fileContent = new String(filedata);
+         out.writeUTF(fileContent.toString());
+         System.out.println("FILE SENT SUCCESSFULLY");
+        }
+      catch (Exception e)
+        {
+          System.out.println(e.getMessage());
+          out.writeUTF("FILE DOESN'T EXISTS");
+        }
+     }
+  }
